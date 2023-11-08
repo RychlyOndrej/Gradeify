bf26501ceeb03e8d35e107d8c9014650a7afa53e
RychlyOndrej <rychly.ondrej@seznam.cz>
2023-11-08 22:23:19 +0100

    Graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 
 dependencies {
 
+    implementation ("com.jjoe64:graphview:4.2.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
--- app/src/main/java/com/example/xmltest/action_home.kt
+++ app/src/main/java/com/example/xmltest/action_home.kt
@@ -1,18 +1,20 @@
 package com.example.xmltest
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.xmltest.ui.theme.ButtonModel
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.helper.StaticLabelsFormatter
+import com.jjoe64.graphview.series.BarGraphSeries
+import com.jjoe64.graphview.series.DataPoint
 
 /*val textView = rootView.findViewById<TextView>(R.id.textView4)
 
@@ -44,6 +46,59 @@ class Window1Fragment : Fragment() {
         val adapter = ButtonAdapter(buttonData)
         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         recyclerView.adapter = adapter
+
+        /*fujky*/
+        // Get a reference to your GraphView from the XML layout
+        val graphView = rootView.findViewById<GraphView>(R.id.graph)
+
+        val series = BarGraphSeries(arrayOf(
+            DataPoint(1.0, 10.0),
+            DataPoint(2.0, 20.0),
+            DataPoint(3.0, 30.0),
+            DataPoint(4.0, 20.0),
+            DataPoint(5.0, 10.0),
+        ))
+
+// Odstranění statických popisků osy X
+        graphView.gridLabelRenderer.isHorizontalLabelsVisible = false
+
+// Nastavení spacing pro mezi sloupci
+        series.spacing = 20 // Zde můžete upravit hodnotu podle svých potřeb
+
+// Nastavení rozsahu osy X tak, aby nebyly uříznuté krajní sloupce
+        graphView.viewport.isXAxisBoundsManual = true
+        graphView.viewport.setMinX(0.5)
+        graphView.viewport.setMaxX(5.5)
+
+// Upravte formátování popisků osy X
+        val customLabelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                if (isValueX) {
+                    // Zde upravte formát popisků osy X podle hodnot datové řady
+                    val xValues = arrayOf(1.0, 2.0, 3.0, 4.0, 5.0)
+                    val index = xValues.indexOf(value)
+                    if (index != -1) {
+                        return (index + 1).toString()
+                    }
+                }
+                return super.formatLabel(value, isValueX)
+            }
+        }
+
+        graphView.gridLabelRenderer.labelFormatter = customLabelFormatter
+
+        graphView.addSeries(series)
+
+
+
+
+        val resetBtn: Button = rootView.findViewById(R.id.Reset_stats_btn)
+        resetBtn.setOnClickListener {
+            // Clear the data in the graph
+            series.resetData(arrayOf())
+        }
+
+        /*endoffujky*/
 
 
         return rootView
