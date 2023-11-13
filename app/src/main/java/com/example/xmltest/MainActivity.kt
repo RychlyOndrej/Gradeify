0402d712af0108c634f694547873b40df1c07e3a
RychlyOndrej <rychly.ondrej@seznam.cz>
2023-11-13 19:21:49 +0100

    small changes

--- app/src/main/java/com/example/xmltest/MainActivity.kt
+++ app/src/main/java/com/example/xmltest/MainActivity.kt
@@ -7,10 +7,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val window1Fragment = Window1Fragment()
-    private val window2Fragment = Window2Fragment()
-    private val window3Fragment = Window3Fragment()
-    private var activeFragment: Fragment = window1Fragment
+    private val homeView = HomeViewImp()
+    private val editView = EditViewImp()
+    private val settingsView = SettingsViewImp()
+    private var activeFragment: Fragment = homeView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
@@ -19,27 +19,27 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        supportFragmentManager.beginTransaction().add(R.id.content_container, window3Fragment, "3").hide(window3Fragment).commit()
-        supportFragmentManager.beginTransaction().add(R.id.content_container, window2Fragment, "2").hide(window2Fragment).commit()
-        supportFragmentManager.beginTransaction().add(R.id.content_container, window1Fragment, "1").commit()
+        supportFragmentManager.beginTransaction().add(R.id.content_container, settingsView, "3").hide(settingsView).commit()
+        supportFragmentManager.beginTransaction().add(R.id.content_container, editView, "2").hide(editView).commit()
+        supportFragmentManager.beginTransaction().add(R.id.content_container, homeView, "1").commit()
 
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
         bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.action_window1 -> {
-                    supportFragmentManager.beginTransaction().hide(activeFragment).show(window1Fragment).commit()
-                    activeFragment = window1Fragment
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(homeView).commit()
+                    activeFragment = homeView
                     true
                 }
                 R.id.action_window2 -> {
-                    supportFragmentManager.beginTransaction().hide(activeFragment).show(window2Fragment).commit()
-                    activeFragment = window2Fragment
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(editView).commit()
+                    activeFragment = editView
                     true
                 }
                 R.id.action_window3 -> {
-                    supportFragmentManager.beginTransaction().hide(activeFragment).show(window3Fragment).commit()
-                    activeFragment = window3Fragment
+                    supportFragmentManager.beginTransaction().hide(activeFragment).show(settingsView).commit()
+                    activeFragment = settingsView
                     true
                 }
                 else -> false
--- app/src/main/java/com/example/xmltest/model/ScaleModel.kt
+++ app/src/main/java/com/example/xmltest/model/ScaleModel.kt
@@ -1,42 +1,51 @@
 package com.example.xmltest
 
-/*
-data class Scale(
-    val name: String,
-    val procentOne: Int,
-    val procentTwo: Int,
-    val procentThree: Int,
-    val procentFour: Int,
-    val procentFive: Int,)
-*/
+import com.jjoe64.graphview.series.DataPoint
 
-interface ScaleModel{
-    fun getAllScales(): List<Scale>
-    fun removeScale(scale: Scale)
-    fun editScale(scale: Scale)
-}
-
-class ScaleModelImp: ScaleModel{
+class ScaleModel {
     private val scales = ArrayList<Scale>()
 
     init {
-        scales.add(Scale("Standart",100,85,60,40,20))
-        scales.add(Scale("Inferno",100,90,80,70,60))
-        scales.add(Scale("Pre-school",100,80,60,30,10))
-        scales.add(Scale("Standdart",100,85,60,40,20))
-        scales.add(Scale("Infferno",100,90,80,70,60))
-        scales.add(Scale("Pre-gschool",100,80,60,30,10))
+        scales.add(Scale("jmeno1", 100, 85,60,40,20))
+        scales.add(Scale("jmeno1sad", 100, 85,60,40,20))
+        scales.add(Scale("jmeno1asdasd", 100, 85,60,40,20))
+        scales.add(Scale("jmenasdsadsadsadsadsado1", 100, 85,60,40,20))
     }
 
-    override fun getAllScales(): List<Scale> {
+    fun onViewCreated() {
+
+
+
+        // Initialize data and set up the view
+        val dataPoints = arrayOf(
+            DataPoint(1.0, 10.0),
+            DataPoint(2.0, 20.0),
+            DataPoint(3.0, 30.0),
+            DataPoint(4.0, 40.0),
+            DataPoint(5.0, 15.0)
+        )
+
+        /*
+        view?.showDataOnGraph(dataPoints)
+        view?.showTextViewContent("Aktuální škálecák")
+
+         */
+    }
+
+    fun onResetBtnClick() {
+        // Clear the data in the graph
+        //view?.showDataOnGraph(arrayOf())
+    }
+
+    fun attachView() {
+        //this.view = view
+    }
+
+    fun detachView() {
+        //this.view = null
+    }
+
+    fun getAllScales(): List<Scale> {
         return scales.toList()
-    }
-
-    override fun removeScale(scale: Scale) {
-        scales.remove(scale)
-    }
-
-    override fun editScale(scale: Scale) {
-        TODO("Not yet implemented")
     }
 }
